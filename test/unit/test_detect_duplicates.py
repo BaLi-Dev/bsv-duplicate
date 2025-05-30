dce0ecb038f2254b920724d9124479b3e4f24f56
Bastian Liljefors Thomsen <bali22@student.bth.se>
2025-05-30 15:55:07 +0200

    First test

--- test/unit/test_detect_duplicates.py
+++ test/unit/test_detect_duplicates.py
@@ -1,7 +1,25 @@
 import pytest
 
+from src.util.detector import detect_duplicates
+
 # develop your test cases here
 
 @pytest.mark.unit
 def test_detect_duplicates():
     assert True
+
+@pytest.mark.unit
+def test_detect_duplicates_with_1_entry():
+    data = '''
+    @article{frattini2023requirements,
+	title={Requirements quality research: a harmonized theory, evaluation, and roadmap},
+	  author={Frattini, Julian and Montgomery, Lloyd and Fischbach, Jannik and Mendez, Daniel and Fucci, Davide and Unterkalmsteiner, Michael},
+	  journal={Requirements Engineering},
+	  pages={1--14},
+	  year={2023},
+	  publisher={Springer},
+	  doi={10.1007/s00766-023-00405-y}
+    }
+    '''
+    duplicates = detect_duplicates(data)
+    assert len(duplicates) == 0
